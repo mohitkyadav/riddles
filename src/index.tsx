4ecb7ee0585daf3c102612c59a904ae983c94e13
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2022-07-01 21:31:49 +0200

    add lint

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-import './sass/main.scss';
+import "./sass/main.scss";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
