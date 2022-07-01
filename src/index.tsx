1de965f19b1c1a0b54b94682f86e8e230ed1f33e
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2022-07-01 21:46:46 +0200

    update font

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+
+import { MainPage } from "./pages";
 import reportWebVitals from "./reportWebVitals";
 
 import "./sass/main.scss";
@@ -10,7 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <MainPage />
   </React.StrictMode>
 );
 
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,7 @@
+export const MainPage: React.FC = () => {
+  return (
+    <div>
+      <h1>MainPage</h1>
+    </div>
+  );
+};
