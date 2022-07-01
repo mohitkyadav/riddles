36a70b319f719ed90a494d2a5864a1c620726f44
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2022-07-01 22:40:10 +0200

    create function to generate boxes

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,11 +1,18 @@
 import { Play, ProblemConfig } from "../../components";
 import { ProblemCfg } from "../../types";
+import { createRandomArray } from "../../utils";
 
 import "./MainPage.scss";
 
 export const MainPage: React.FC = () => {
   const handleConfigChange = (config: ProblemCfg) => {
     console.log(config);
+
+    console.log(
+      "🚀 ~ file: MainPage.tsx ~ line 12 ~ handleConfigChange ~ createRandomArray(config.noOfMen);",
+      createRandomArray(config.noOfMen)
+    );
+    createRandomArray(config.noOfMen);
   };
 
   return (
---
+++ src/utils/index.ts
@@ -0,0 +1,29 @@
+export const isEmpty = (value: any) => {
+  return value === undefined || value === null || value === "";
+};
+
+/**
+ * @description Creates an array of length `size` filled with a unique random value between `0` and `size - 1`
+ * @param size The length of the array to create
+ * @returns An array of length `size` filled with a unique random value between `0` and `size - 1`
+ * @example
+ * ```ts
+ * const array = createRandomArray(10);
+ * console.log(array); // [2, 3, 4, 5, 6, 7, 8, 9, 0, 1]
+ * ```
+ */
+export const createRandomArray = (size: number) => {
+  const array: Array<number> = [];
+
+  for (let i = 0; i < size; i++) {
+    const newNumber = Math.floor(Math.random() * size);
+
+    if (array.includes(newNumber)) {
+      i--;
+    } else {
+      array.push(newNumber);
+    }
+  }
+
+  return array;
+};
