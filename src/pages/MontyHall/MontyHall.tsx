11780af5a0850b312c1e242c6d1286107b31f38b
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2022-07-03 20:17:35 +0200

    Add style to the doors

--- src/pages/MontyHall/MontyHall.tsx
+++ src/pages/MontyHall/MontyHall.tsx
@@ -1,7 +1,71 @@
+import { useState } from "react";
+import { Footer } from "../../components";
+import "./MontyHall.scss";
+
 export const MontyHall: React.FC = () => {
+  const noOfDoors = 3;
+  const noOfSimulations = 3;
+
+  const [winPercentage, setWinPercentage] = useState(0);
+  const [doorWithCar, setDoorWithCar] = useState(0);
+  const [selectedDoor, setSelectedDoor] = useState(0);
+
+  const startSimulation = async () => {
+    const noOfWins = await simulate();
+
+    console.log(
+      "🚀 ~ file: MontyHall.tsx ~ line 19 ~ startSimulation ~ noOfWins",
+      noOfWins
+    );
+
+    const winPercentage = (noOfWins / noOfSimulations) * 100;
+    setWinPercentage(winPercentage);
+  };
+
+  const getOpenedDoors = (selectedDoor: number, doorWithCar: number) => {
+    const openedDoors = [];
+  };
+
+  const simulate = async () => {
+    let noOfWins = 0;
+    const doorWithCar = Math.floor(Math.random() * noOfDoors) + 1;
+    setDoorWithCar(doorWithCar);
+
+    for (let i = 0; i < noOfSimulations; i++) {
+      await new Promise((r) => setTimeout(r, 2000));
+
+      const selectedDoor = Math.floor(Math.random() * noOfDoors) + 1;
+      const openedDoors = getOpenedDoors(selectedDoor, doorWithCar);
+
+      console.log(doorWithCar, selectedDoor, openedDoors);
+    }
+
+    return noOfWins;
+  };
+
   return (
-    <div>
-      <h1>MontyHall</h1>
+    <div className="mh">
+      <h1>The Monty Hall Problem</h1>
+
+      <div className="mh__container">
+        <div className="mh__container__doors">
+          {Array.from({ length: noOfDoors }).map((_, index) => (
+            <div className="mh__container__doors__door" key={index}>
+              <div className="mh__container__doors__door__content">
+                <div className="mh__container__doors__door__content__text">
+                  {index + 1}
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className="mh__container__controls">
+          <button onClick={startSimulation}>Start Simulation</button>
+        </div>
+      </div>
+
+      <Footer />
     </div>
   );
 };
